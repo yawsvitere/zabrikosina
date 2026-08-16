5791621494e3f592de3eeeb934fbabb18ade7f14
yawsvitere <yawsvitere@gmail.com>
2026-08-16 16:28:40 +0300

    feat: add Terms of Service page and update routing; adjust styles and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home';
 import { Contacts } from './contacts';
+import { TermsOfService } from './terms-of-service';
 
 export const routes: Routes = [
   { path: '', title: 'zabrikosina', component: Home },
   { path: 'contacts', title: 'Контакты', component: Contacts },
+  { path: 'terms-of-service', title: 'zabrikosina', component: TermsOfService },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/terms-of-service.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'terms-of-service',
+  standalone: true,
+  templateUrl: './terms-of-service.html',
+  styleUrls: ['./terms-of-service.css']
+})
+export class TermsOfService {}
